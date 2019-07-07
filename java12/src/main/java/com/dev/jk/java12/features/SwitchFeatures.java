6d3767e2c659af5691c6f1f1e13e18b359f17d4d
jayasekhar <donotreplay@jayasekhar.com>
2019-07-07 12:13:03 +0530

    Java 12 features

---
+++ java12/src/main/java/com/dev/jk/java12/features/SwitchFeatures.java
@@ -0,0 +1,47 @@
+package com.dev.jk.java12.features;
+
+public class SwitchFeatures {
+
+    public String oldSwitch()
+    {
+        final int integer = 5;
+        String numericString = null;
+        switch (integer)
+        {
+            case 1 :
+                numericString = "one";
+                break;
+            case 2 :
+                numericString = "two";
+                break;
+            case 3:
+                numericString = "three";
+                break;
+            case 4:
+                numericString = "greater than 3";
+                break;
+            case 5:
+                numericString = "greater than 3";
+                break;
+            case 6:
+                numericString = "greater than 3";
+            default:
+                numericString = "N/A";
+        }
+        return numericString;
+    }
+
+    public String newSwitch()
+    {
+        final int integer = 5;
+        String numericString = switch (integer)
+                                    {
+                                        case 1          ->      "one";
+                                        case 2          ->      "two";
+                                        case 3          ->      "three";
+                                        case 4, 5, 6, 7 ->      "greater than 3";
+                                        default         ->      "N/A";
+                                    };
+        return numericString;
+    }
+}
---
+++ java12/src/test/java/com/dev/jk/java12/SwitchFeaturesTest.java
@@ -0,0 +1,31 @@
+package com.dev.jk.java12;
+
+import com.dev.jk.java12.features.SwitchFeatures;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+@Ignore
+public class SwitchFeaturesTest {
+
+    private SwitchFeatures switchFeatures;
+
+    @Before
+    public void setUp() {
+        switchFeatures = new SwitchFeatures();
+    }
+
+    @Test
+    public void oldSwitch() {
+        String oldSwitch = switchFeatures.oldSwitch();
+        Assert.assertEquals("greater than 3", oldSwitch);
+    }
+
+    @Test
+    public void newSwitch() {
+        String newSwitch = switchFeatures.oldSwitch();
+        Assert.assertEquals("greater than 3", newSwitch);
+    }
+
+}
